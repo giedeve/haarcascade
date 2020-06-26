89f7585549e444211bc26261265da1198526236f
Gie <gieczarny@gmail.com>
2020-06-26 16:59:47 +0200

    add file img and fix code cascade

--- cascade.py
+++ cascade.py
@@ -2,23 +2,19 @@ import numpy as np
 import cv2
 
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-cap = cv2.VideoCapture(0)
+img = cv2.imread("img/trump.jpg")
 
-while (True):
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    for (x, y, w, h) in faces:
-        face = img[y:y + h, x:x + w]
-        face = cv2.blur(face, ((w // 5), (h // 5)))
-        img[y:y + h, x:x + w] = face
+for (x, y, w, h) in faces:
+    face = img[y:y+h, x:x+w]
+    face = cv2.blur(face, ((w // 5), (h // 5)))
+    img[y:y+h, x:x+w] = face
 
-    cv2.imshow('img', img)
-    if cv2.waitKey(30) & 0xFF == ord('q'):
-        break
 
-cap.release()
+cv2.imshow('img', img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-
